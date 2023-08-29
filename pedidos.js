ea574c262b4fdf006052c1f508dcac5f4b8bb33e
MarcusViniciusCOli-Dev <125603205+MarcusViniciusCOli-Dev@users.noreply.github.com>
2023-08-29 14:04:57 -0300

    Add files via upload
    commit 29/08/2023

---
+++ pedidos.js
@@ -0,0 +1,19 @@
+function abrirModal(){
+    let modal = document.querySelector(".modal")
+    let bg = document.querySelector(".bg")
+
+    modal.style.display = 'block';
+    bg.style.display = 'block';
+}
+
+function fechar(){
+    let modal = document.querySelector(".modal")
+    let bg = document.querySelector(".bg")
+
+    modal.style.display = 'none';
+    bg.style.display = 'none';
+}
+
+function enviarCozinha(){
+    
+}
